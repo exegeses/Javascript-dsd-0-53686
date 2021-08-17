4ea91f2d0ba7b69f7410b0e14ef1c1f4f5d61065
Marcos <exegeses@gmail.com>
2021-08-17 10:53:08 -0300

    Funciones , declaración + práctica mostrar/ocultar

---
+++ clase3/js/funciones.js
@@ -0,0 +1,19 @@
+//ubicamos elemento dentro del DOM
+var caja = document.getElementById('caja');
+
+//cambiamos altura, ancho, color de fondo
+/*
+    caja.style.height = '150px';
+    caja.style.width = '95px';
+    caja.style.backgroundColor = '#B0E3FC';
+ */
+
+//declaramos función
+function modificarAtributos()
+{
+    caja.style.height = '150px';
+    caja.style.width = '95px';
+    caja.style.backgroundColor = '#B0E3FC';
+}
+// llamado a ejecución
+//modificarAtributos();

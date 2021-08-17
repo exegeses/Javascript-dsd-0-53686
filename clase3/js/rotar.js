237883777dfa2fe4e986dbdc7ca3f85dbef44ba5
Marcos <exegeses@gmail.com>
2021-08-17 12:04:19 -0300

    Ejemplo rotar caja con setInterval

---
+++ clase3/js/rotar.js
@@ -0,0 +1,14 @@
+//ubicar caja
+var caja = document.getElementById('caja');
+    caja.style.position = 'relative';
+    caja.style.top = '100px';
+var grados = 0;
+
+function rotar()
+{
+    grados = grados + 1;
+    caja.style.transform = 'rotate( '+grados+'deg )';
+}
+
+rotar();
+setInterval( rotar, 100 );

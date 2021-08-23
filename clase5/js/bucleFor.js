f112bb29c84a039a1197fcea278ddbade44bd1e5
Marcos <exegeses@gmail.com>
2021-08-23 10:26:50 -0300

    Material imagenes clase 5

---
+++ clase5/js/bucleFor.js
@@ -0,0 +1,10 @@
+    var autos = [
+                    'Aston Martin', 'Lotus', 'MacLaren',
+                    'Rover', 'Bugatti', 'Lamborghini',
+                    'Bentley', 'Vauxhal', 'Pagani'
+                ];
+    var cantidad = autos.length;
+
+    for( var n = 0; n < cantidad; n++ ){
+        document.write( autos[n] + '<br>' );
+    }
